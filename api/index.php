0cd1a75b26a591213ae9b55647eae2a0e63a617f
Rangga Aditya Pratama <ranggaaditya200371@gmail.com>
2025-10-04 19:20:45 +0700

    Commit Bio3

--- api/index.php
+++ api/index.php
@@ -1,3 +1,7 @@
 <?php
 
+// Pastikan jalur ke file autoload.php dan public/index.php sudah benar
+require __DIR__ . '/../vendor/autoload.php';
+
+// Jika Anda menggunakan Laravel, biasanya cukup panggil public/index.php
 require __DIR__ . '/../public/index.php';
